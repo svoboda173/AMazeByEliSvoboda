effa6466f81887dea83c55a0f083f402a1cf70bd
Eli Svoboda <essvoboda@email.wm.edu>
2022-06-24 16:31:56 -0400

    First commit. Folders set up, no functionality yet.

---
+++ app/src/main/java/edu/wm/cs/cs301/EliSvoboda/gui/AMazeActivity.java
@@ -0,0 +1,16 @@
+package edu.wm.cs.cs301.EliSvoboda.gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.wm.cs.cs301.EliSvoboda.R;
+
+public class AMazeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
