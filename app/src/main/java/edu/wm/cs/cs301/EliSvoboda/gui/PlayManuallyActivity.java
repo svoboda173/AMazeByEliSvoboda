77b0b03d19103d3ed891631bfb81548b04032c17
Eli Svoboda <essvoboda@email.wm.edu>
2022-06-24 17:00:51 -0400

    All 5 activities created. UI must now be created.

---
+++ app/src/main/java/edu/wm/cs/cs301/EliSvoboda/gui/PlayManuallyActivity.java
@@ -0,0 +1,14 @@
+package edu.wm.cs.cs301.EliSvoboda.gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayManuallyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_manually);
+    }
+}
