efac64cbd14d23a2b48c468b26c37a4fceb0487c
Eli Svoboda <essvoboda@email.wm.edu>
2022-06-26 21:32:20 -0400

    Working switch from title to generating activity, and skeleton generating thread code

--- app/src/main/java/edu/wm/cs/cs301/EliSvoboda/gui/AMazeActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/EliSvoboda/gui/AMazeActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import edu.wm.cs.cs301.EliSvoboda.R;
 
@@ -25,11 +27,41 @@ public class AMazeActivity extends AppCompatActivity {
         Spinner modeSpinner = findViewById(R.id.modeSpinner);
         Button retryButton = findViewById(R.id.retryButton);
 
+        boolean includeRooms = false;
+        int currentLevel = 0;
+
+
+
         escapeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(AMazeActivity.this, GeneratingActivity.class);
                 startActivity(intent);
             }
         });
+
+        sizeBar.setMax(15);
+        sizeBar.setMin(0);
+        sizeBar.setProgress(0);
+        sizeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar sizeBar, int i, boolean b) {
+               currentLevel = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.v("AMazeActivity", "You changed the size of the maze to " + currentLevel);
+                Toast.makeText(AMazeActivity.this, "You changed the size of the maze to " + currentLevel, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void roomsClicked() {
+
     }
 }
--- app/src/main/java/edu/wm/cs/cs301/EliSvoboda/gui/GeneratingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/EliSvoboda/gui/GeneratingActivity.java
@@ -2,13 +2,19 @@ package edu.wm.cs.cs301.EliSvoboda.gui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import edu.wm.cs.cs301.EliSvoboda.R;
 
 public class GeneratingActivity extends AppCompatActivity {
+
+    private static int progress;
+    private Handler handler = new Handler();
+    private int progressStatus = 0; //Method adopted from http://www.java2s.com/Code/Android/UI/UsingThreadandProgressbar.htm
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,34 @@ public class GeneratingActivity extends AppCompatActivity {
 
         ProgressBar generationProgress = findViewById(R.id.progressBar);
         TextView progressGeneration = findViewById(R.id.progressGenerating);
+
+        int progress = 0;
+        generationProgress.setMax(100);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus = doWork();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            generationProgress.setProgress(progressStatus);
+                            progressGeneration.setText(progressStatus);
+                        }
+                    });
+                }
+                Intent intent = new Intent(GeneratingActivity.this, GeneratingActivity.class);
+                startActivity(intent);
+            }
+        }).start();
+    }
+
+    private int doWork() {
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return ++progressStatus;
     }
 }
